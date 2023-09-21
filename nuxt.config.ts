da81a642aca87ec3742b9c317c2cfbc8fd2b6684
Tapawingo <tapawingogamer@gmail.com>
2023-09-21 22:50:37 +0200

    new deployment config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@vueuse/nuxt', 'nuxt-icon'],
-  nitro: {
-    prerender: {
-        failOnError: false
-    }
-  }
+  modules: ['@vueuse/nuxt', 'nuxt-icon']
 })
