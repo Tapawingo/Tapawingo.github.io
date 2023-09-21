6a1094fda1e5b2574b91e2d53f40f912ed3212f0
Tapawingo <tapawingogamer@gmail.com>
2023-09-21 23:43:29 +0200

    Fixed bad config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@vueuse/nuxt', 'nuxt-icon'],
-  app: {
-    baseURL: '/',
-    buildAssetsDir: 'assets'
-  }
+  modules: ['@vueuse/nuxt', 'nuxt-icon']
 })
