4bb3588aa0b1a7fdac2808a32518b44828754397
Tapawingo <tapawingogamer@gmail.com>
2023-09-21 23:14:14 +0200

    Deployment changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@vueuse/nuxt', 'nuxt-icon']
+  modules: ['@vueuse/nuxt', 'nuxt-icon'],
+  generate:{
+    nojekyll: true
+  },
+  ssr: false,
+  target: "static",
 })
