e718760378bc64589f716db0e19bbb9e2e01dd08
Tapawingo <tapawingogamer@gmail.com>
2023-09-22 02:42:36 +0200

    Minor Startpage Cleanup

---
+++ components/StartPageNav.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav>
+        <div class="nav-container">
+            <ul v-for="section in links">
+                <a class="nav-subtitle">{{ section.title }}</a>
+                <li v-for="link in section.links">
+                    <a :href="link.href">{{ link.mask }}</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script lang="ts" setup>
+    const props = defineProps({
+        links: {
+            type: Array,
+            default: []
+        }
+    })
+</script>
--- pages/startpage.vue
+++ pages/startpage.vue
@@ -1,54 +1,19 @@
 <template>
     <!-- Welcome screen -->
-    <div id="main_window">
-        <img id=leftImg src="/img/widgetCover.jpg" class="image">
-        <div id="content">
-            <h1 class="title">Tapawingo</h1>
-            <div ref="date"></div>
+    <div id="main_window" class="widget">
+        <img class="widget-picture" src="/img/widgetCover.jpg">
+        <div class="widget-content">
+            <h1 class="widget-title">Tapawingo</h1>
+            <div class="widget-date">{{ date }}</div>
 
-            <div id="web_search">
+            <div class="widget-search">
                 <form action="https://duckduckgo.com/" method="GET" autocomplete="off">
                     <label for="q">></label>
                     <input autofocus id="q" name="q" type="search" placeholder="search with duckduckgo...">
                 </form>
             </div>
 
-            <nav>
-                <div id=links_container>
-                    <ul>
-                        <a class="subtitle">Uni</a>
-                        <li><a href="https://innsida.ntnu.no/start#/feed/">Innsida NTNU</a></li>
-                        <li><a href="https://ntnu.blackboard.com/ultra//">Blackboard</a></li>
-                        <li><a href="https://www.microsoft365.com/apps?auth=2">Office 365</a></li>
-                        <li><a href="https://outlook.office.com/mail/">Mail</a></li>
-                        <li><a href="https://drive.google.com/drive/folders/1UZXVBcLjBzZ9mgY7Z2i4k6EdZC7dX_W_">Books</a></li>
-                    </ul>
-                    <ul>
-                        <a class="subtitle">Media</a>
-                            <li><a href="https://www.netflix.com/browse">Netflix</a></li>
-                            <li><a href="https://www.youtube.com/">YouTube</a></li>
-                            <li><a href="https://www.anix.to/">ANIX</a></li>
-                    </ul>
-                    <ul>
-                        <a class="subtitle">4Chan</a>
-                            <li><a href="https://4chan.org/wsg/">/wsg/</a></li>
-                            <li><a href="https://4chan.org/k/">/k/</a></li>
-                            <li><a href="https://4chan.org/g/">/g/</a></li>
-                    </ul>
-                    <ul>
-                        <a class="subtitle">Devshits</a>
-                            <li><a href="https://github.com/">GitHub</a></li>
-                            <li><a href="https://stackoverflow.com/">Stack Overflow</a></li>
-                            <li><a href="https://ocw.mit.edu/search/?d=Electrical%20Engineering%20and%20Computer%20Science&s=department_course_numbers.sort_coursenum">OpenCourseware</a></li>
-                    </ul>
-                    <ul>
-                        <a class="subtitle">Others</a>
-                            <li><a href="https://catbox.moe/">Catbox</a></li>
-                            <li><a href="http://localhost:3000">localhost</a></li>
-                            <li><a href="https://www.dev.to/">DEV</a></li>
-                    </ul>
-                </div>
-            </nav>
+            <StartPageNav v-bind="nav" />
         </div>
     </div>
 </template>
@@ -56,24 +21,65 @@
 <script lang="ts" setup>
     import '~/assets/styles/startpage/main.scss';
 
-    const date: Ref<null | HTMLElement> = ref(null);
-    const updateDate = async () => {
-        function doDate() {
+    const date: Ref<string> = ref('');
+
+    const nav = {
+        links: [
+            {
+                title: 'Uni',
+                links: [
+                    { href:'https://innsida.ntnu.no/start#/feed/', mask: 'Innsida NTNU' },
+                    { href:'https://ntnu.blackboard.com/ultra/', mask: 'Blackboard' },
+                    { href:'https://www.microsoft365.com/apps?auth=2', mask: 'Office 365' },
+                    { href:'https://outlook.office.com/mail/', mask: 'Mail' },
+                    { href:'https://drive.google.com/drive/folders/1UZXVBcLjBzZ9mgY7Z2i4k6EdZC7dX_W_', mask: 'Books' }
+                ]
+            },
+            {
+                title: 'Media',
+                links: [
+                    { href:'https://www.netflix.com/browse', mask: 'Netflix' },
+                    { href:'https://www.youtube.com/', mask: 'YouTube' },
+                    { href:'https://www.anix.to/', mask: 'ANIX' }
+                ]
+            },
+            {
+                title: '4Chan',
+                links: [
+                    { href:'https://4chan.org/wsg/', mask: '/wsg/' },
+                    { href:'https://4chan.org/k/', mask: '/k/' },
+                    { href:'https://4chan.org/g/', mask: '/g/' }
+                ]
+            },
+            {
+                title: 'Devshits',
+                links: [
+                    { href:'https://github.com/', mask: 'GitHub' },
+                    { href:'https://stackoverflow.com/', mask: 'Stack Overflow' },
+                    { href:'https://ocw.mit.edu/search/?d=Electrical%20Engineering%20and%20Computer%20Science&s=department_course_numbers.sort_coursenum', mask: 'OpenCourseWare' }
+                ]
+            },
+            {
+                title: 'Others',
+                links: [
+                    { href:'https://catbox.moe/', mask: 'Catbox' },
+                    { href:'http://localhost', mask: 'localhost' },
+                    { href:'https://www.dev.to/', mask: 'DEV' }
+                ]
+            }
+        ]
+    }
+
+    onMounted(() => {
+        setInterval(() => {
             var str = "";
             var months = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
             var clock = new Date();
             var now = new Date();
 
             str += months[clock.getMonth()] + " " + now.getDate() + ", " + clock.getFullYear() + " | " + ("0" + now.getHours()).slice(-2) +":" + ("0" + now.getMinutes()).slice(-2) + ":" + ("0" + now.getSeconds()).slice(-2);
-            if (date.value) {
-                date.value.innerHTML = str;
-            }
-        }
-        setInterval(doDate, 1000);
-        doDate();
-    };
 
-    onMounted(() => {
-        updateDate();
+            date.value = str;
+        }, 1000);
     })
 </script>
