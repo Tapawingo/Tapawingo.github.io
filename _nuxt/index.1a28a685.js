ba22364f08d3419075a205240241f4a329f34f61
Tapawingo <tapawingogamer@gmail.com>
2024-02-05 15:08:54 +0100

    Updates

---
+++ _nuxt/index.1a28a685.js
@@ -0,0 +1,1 @@
+import{r as u,J as c,j as f,K as l,L as p}from"./entry.9857394d.js";function d(t,a){const e=u({});return c(()=>{const s=l(t),{title:n,titleTemplate:o,...r}=s;e.value={title:n,titleTemplate:o,meta:p(r)}}),f(e,a)}export{d as u};
